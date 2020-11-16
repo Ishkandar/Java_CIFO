de2744923fa63a0974cbcc99ced1969d70572532
Alex <alex.dopico@gmail.com>
2020-11-16 13:31:56 +0100

    simple h2-spring jpa example

---
+++ customersh2/src/main/java/customersh2/Customer.java
@@ -0,0 +1,41 @@
+package customersh2;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Customer {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	private String firstName;
+	private String lastName;
+	
+	protected Customer() {}
+	
+	public Customer(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+	public Long getId() {
+		return this.id;
+	}
+	
+	public String getFirstName() {
+		return this.firstName;
+	}
+	
+	public String getLastName() {
+		return this.lastName;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format(
+				"Customer [id = %d, firstName = '%s', lastName = '%s']", 
+				id, firstName, lastName);
+	}
+}
